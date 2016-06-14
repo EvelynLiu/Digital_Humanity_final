f802bf8d05b04122c18fecadae3836248c0c577c
ray <ray20013247777@gmail.com>
2016-06-14 20:35:30 +0800

    add query.php add scroll function.php

---
+++ test.php
@@ -0,0 +1,46 @@
+﻿<html>
+	<link rel="stylesheet" href="index.css" type="text/css" >
+
+
+<?php
+	include("dbManager.php");
+	
+	$db = new dbManager;
+	
+/*
+	$data = $db -> query_book(1);
+	for($i=0; $i<count($data);$i++){
+		echo $data[$i]['CONTENT']."<br>";
+	}
+*/
+
+	$data = $db -> query_book(1);
+	$title = "";
+	$content = "";
+	
+	for($i=0; $i<count($data);$i++){
+		
+		if($data[$i]['TITLE'] == $title) {
+			
+			$content.=$data[$i]['CONTENT']."<br>";
+			
+			
+		} else {
+			if( $title != "") {
+				echo "<div class='block' name='$title'>";
+				echo "<p class='title'>$title</p>";
+				echo "$content";
+				echo "<br></div>";
+			}
+			$title = "";
+			$content = "";
+			
+			$title = $data[$i]['TITLE'];
+			$content.=$data[$i]['CONTENT']."<br>";
+			
+		}
+	}
+	
+?>
+
+</html>
