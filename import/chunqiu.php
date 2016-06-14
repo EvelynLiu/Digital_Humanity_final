4eae535c89384b651affac6732839a55f27423e1
ray <ray20013247777@gmail.com>
2016-06-14 16:16:28 +0800

    add import file add index.php add dbManager.php

---
+++ import/chunqiu.php
@@ -0,0 +1,36 @@
+<?php
+	$data = $db -> query_book(1);
+	$title = "";
+	$content = "";
+	// print_r($data);
+	// echo "<a href=\"\" class=\"abgne_gotoheader\">Go To Target</a>";
+	for($i=0; $i<count($data);$i++){
+		$item = $data[$i];
+		$start = $item['START_YEAR'];
+		$end = $item['END_YEAR'];
+		$classYear = "";
+		for($j = -1; $j < substr($end, 5,2) - substr($start, 5,2); $j++){
+			$temp = substr($start, 5, 2) + $j + 1;
+			if ((substr($start, 5, 2) + $j + 1) < 10) {
+				$temp = "0".$temp;
+			}
+			$classYear.=" ".substr($start, 0,5).$temp;
+		}
+		if($item['TITLE'] == $title) {
+			$content.="<li><a href=\"#\" class=\"text".$classYear."\" id=\"".$start."\">".$data[$i]['CONTENT']."</a></li>";		
+		} else {
+			if( $title != "") {
+				echo "<div class='block $title' name='$title'>";
+				echo "<p class='title'>$title</p>";
+				echo "$content";
+				echo "<br></div>";
+			}
+			$title = "";
+			$content = "";
+			
+			$title = $data[$i]['TITLE'];
+			$content.="<li><a href=\"\" class=\"text".$classYear."\" id=\"".$start."\">".$data[$i]['CONTENT']."</a></li>";
+			
+		}
+	}
+?>
--- js/content.js
+++ js/content.js
@@ -16,12 +16,15 @@ $(document).on('click', '.dropdown-menu li a', function () {
 });
 
 function show_page() {
+	
 	var el = document.getElementById('books');
 	var input = el.getElementsByTagName('input');
 	var count = 0;
 	var book_names = [];
 	var checked = [];
 	var main = document.getElementById('main');
+	
+	
 	for (var i = 0, len = input.length; i < len; i++){
         if (input[i].type === 'checkbox'){
             book_names[i] = input[i].value;
@@ -32,23 +35,79 @@ function show_page() {
             console.log(book_names[i]);
         }
     }
+	
+	
     console.log(count);
     if (count === 0){ //only春秋
-    	main.innerHTML = "<div class=\"col-sm-10 col-md-10 content\"><div class=\"row\"><div class=\"col-sm-12 content\"><h1 class=\"page-header\">春秋</h1><ul class=\"nav nav-sidebar\"><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li></ul></div></div></div>";
+		$( $('.book')[0] ).attr("class", "book col-sm-12 content");
+		
+    	$( $('.book')[1] ).attr("class", "hidden");
+		$( $('.book')[2] ).attr("class", "hidden");
+		$( $('.book')[3] ).attr("class", "hidden");
     }
     else if (count === 1){ //col-sm-6 * 2 => checked[0]
-    	main.innerHTML = "<div class=\"col-sm-10 col-md-10\"><div class=\"row\"><div class=\"col-sm-6 content\"><h1 class=\"page-header\">春秋</h1><ul class=\"nav nav-sidebar\"><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li></ul></div><div class=\"col-sm-6 content\"><h1 class=\"page-header\">"+checked[0]+"</h1><ul class=\"nav nav-sidebar\"><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li></ul></div></div></div>";
+		$( $('.book')[0] ).attr("class", "book col-sm-6 content");
+		
+		if(checked[0] == "左傳") {
+			$( $('.book')[1] ).attr("class", "book col-sm-6 content");
+			
+			$( $('.book')[2] ).attr("class", "book col-sm-6 content hidden");
+			$( $('.book')[3] ).attr("class", "book col-sm-6 content hidden");
+		}
+				
+		else if (checked[0] == "公羊傳"){
+			$( $('.book')[2] ).attr("class", "book col-sm-6 content");
+			
+			$( $('.book')[1] ).attr("class", "book col-sm-6 content hidden");
+			$( $('.book')[3] ).attr("class", "book col-sm-6 content hidden");
+		}
+		else if (checked[0] == "穀梁傳") {
+			$( $('.book')[3] ).attr("class", "book col-sm-6 content");
+			
+			$( $('.book')[1] ).attr("class", "book col-sm-6 content hidden");
+			$( $('.book')[2] ).attr("class", "book col-sm-6 content hidden");
+		}
     }
     else if (count === 2){ //col-sm-4 * 3 => checked[0][1]
-    	main.innerHTML = "<div class=\"col-sm-10 col-md-10\"><div class=\"row\"><div class=\"col-sm-4 content\"><h1 class=\"page-header\">春秋</h1><ul class=\"nav nav-sidebar\"><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li></ul></div><div class=\"col-sm-4 content\"><h1 class=\"page-header\">"+checked[0]+"</h1><ul class=\"nav nav-sidebar\"><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li></ul></div><div class=\"col-sm-4 content\"><h1 class=\"page-header\">"+checked[1]+"</h1><ul class=\"nav nav-sidebar\"><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li></ul></div></div></div>";
+    	$( $('.book')[0] ).attr("class", "book col-sm-4 content");
+		
+		if(checked[0] == "左傳" && checked[1] == "公羊傳") {
+			$( $('.book')[1] ).attr("class", "book col-sm-4 content");
+			$( $('.book')[2] ).attr("class", "book col-sm-4 content");
+			
+			$( $('.book')[3] ).attr("class", "book col-sm-4 content hidden");
+		} 
+		else if(checked[0] == "左傳" && checked[1] == "穀梁傳") {
+			$( $('.book')[1] ).attr("class", "book col-sm-4 content");
+			$( $('.book')[3] ).attr("class", "book col-sm-4 content");
+			
+			$( $('.book')[2] ).attr("class", "book col-sm-4 content hidden");
+		}
+		else if(checked[0] == "公羊傳" && checked[1] == "穀梁傳") {
+			$( $('.book')[1] ).attr("class", "book col-sm-4 content hidden");
+			
+			$( $('.book')[2] ).attr("class", "book col-sm-4 content");
+			$( $('.book')[3] ).attr("class", "book col-sm-4 content");
+		}
     }
     else if (count === 3){ //col-sm-3 * 4 => checked[0][1][2]
-    	main.innerHTML = "<div class=\"col-sm-10 col-md-10\"><div class=\"row\"><div class=\"col-sm-3 content\"><h1 class=\"page-header\">春秋</h1><ul class=\"nav nav-sidebar\"><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li></ul></div><div class=\"col-sm-3 content\"><h1 class=\"page-header\">"+checked[0]+"</h1><ul class=\"nav nav-sidebar\"><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li></ul></div><div class=\"col-sm-3 content\"><h1 class=\"page-header\">"+checked[1]+"</h1><ul class=\"nav nav-sidebar\"><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li></ul></div><div class=\"col-sm-3 content\"><h1 class=\"page-header\">"+checked[2]+"</h1><ul class=\"nav nav-sidebar\"><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li></ul></div></div></div>";
+    	$( $('.book')[0] ).attr("class", "book col-sm-3 content");
+		
+    	$( $('.book')[1] ).attr("class", "book col-sm-3 content");
+		$( $('.book')[2] ).attr("class", "book col-sm-3 content");
+		$( $('.book')[3] ).attr("class", "book col-sm-3 content");
     }
+	/*
     else if (count === 4){ //col-sm-2 * 5 => checked[0][1][2]
-    	main.innerHTML = "<div class=\"col-sm-2 content\"><h1 class=\"page-header\">春秋</h1><ul class=\"nav nav-sidebar\"><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li></ul></div><div class=\"col-sm-2 content\"><h1 class=\"page-header\">左傳</h1><ul class=\"nav nav-sidebar\"><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li></ul></div><div class=\"col-sm-2 content\"><h1 class=\"page-header\">公羊傳</h1><ul class=\"nav nav-sidebar\"><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li></ul></div><div class=\"col-sm-2 content\"><h1 class=\"page-header\">穀梁傳</h1><ul class=\"nav nav-sidebar\"><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li></ul></div><div class=\"col-sm-2 content\"><h1 class=\"page-header\">春秋經解</h1><ul class=\"nav nav-sidebar\"><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li><li><a href=\"#\">...</a></li></ul></div>";
-    }
+    	$( $('.book')[0] ).attr("class", "book col-sm-3 content");
+		
+    	$( $('.book')[1] ).attr("class", "book col-sm-3 content");
+		$( $('.book')[2] ).attr("class", "book col-sm-3 content");
+		$( $('.book')[3] ).attr("class", "book col-sm-3 content");
+    }*/
 }
+
+
 /*
 "<div class=\"col-sm-10 col-md-10\">
 	<div class=\"row\">
